fac0b122e32a48b4265af15d846842ec21c85dc4
Joshua Meyer <joshuameyer@Joshuas-MacBook-Pro.local>
2024-10-24 13:28:24 -0600

    fix on package

--- app/Intro.tsx
+++ app/Intro.tsx
@@ -1,6 +1,6 @@
 export default function Intro() {
   return (
-    <div className="grid grid-rows-1 items-center justify-items-center h-[calc(100vh-36px)]">
+    <div className="grid grid-rows-1 items-center justify-items-center h-[calc(100vh-106px)]">
       <div className="flex flex-col gap-8 items-center justify-items-center">
         <h1 className="flex flex-col items-center mb-4 gap-3 text-4xl sm:text-5xl font-extrabold leading-none tracking-tight text-gray-300 dark:text-white select-none">Hello, I&apos;m <span className="bg-gradient-to-r from-blue-700 via-purple-300 to-purple-600 text-transparent bg-clip-text text-5xl sm:text-6xl">Joshua Meyer</span></h1>
         <p> Software Engineer | Backend | DevOps </p>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
     <div className="min-h-screen dotted-background overflow-scroll	font-[family-name:var(--font-geist-sans)]">
       <Intro/>
       {/* <Skills/> */}
-      <footer className="pb-5 row-start-3 flex gap-8 flex-wrap items-center justify-center">
+      <footer className="pb-5 row-start-3 flex flex-col gap-8 flex-wrap items-center justify-center">
+        <div className="flex gap-8">
+        <a href="https://github.com"><img src="icons8-github.svg" height={38} width={38}></img></a>
+        <a href="https://github.com"><img src="icons8-linkedin.svg" height={38} width={38}></img></a>
+        </div>
         <p className="text-xs text-gray-200">© 2024 Joshua Meyer</p>
       </footer>
     </div>
